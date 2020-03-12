3a66512bcaa4e1229628fe7b06daf5e5ceec989a
Siphamandla <mr.smehlomakulu@gmail.com>
2020-03-12 05:05:16 +0200

    code cleanup in the taskSlide component

--- src/components/SingleTask/TaskSlide/TaskSlide.js
+++ src/components/SingleTask/TaskSlide/TaskSlide.js
@@ -3,13 +3,22 @@ import classes from './TaskSlide.module.css';
 
 class TaskSlide extends Component {
 
+    // Resize the width of the cards to match that of the slide container
+    // after the component has mounted
+    resizeCards = () => { 
+        let cardContainerWith = document.querySelector('#cardContainer').offsetWidth
+        let cards = document.querySelector('#cardContainer').firstChild.children;
+        for (let i = 0; i < cards.length; i++) cards[i].style.width = cardContainerWith + 'px';
+    }
     
 
     render = () => {
-        let tasks = this.props.tasks;
-        // let key = props.key;
 
-        let taskList = Object.keys(this.props.tasks).map( key => {
+        let tasks = this.props.tasks;
+        // let key = props.key; I will use this key when I want to delete tasks from this page
+
+        //make task cards from the tasks object
+        let taskPage = Object.keys(tasks).map( key => {
             return (
                 <div key={key} className={classes.taskCard}>
                     <h3>{tasks[key].title}</h3>
@@ -20,29 +29,22 @@ class TaskSlide extends Component {
             );
         });
 
+        // Moves the strip to left or right depending on the slide that has to be shown or that is active.
+        const slideStripStyle = {
+            'transform': `translateX(-${this.props.current*(100/this.props.numberOfTasks)}%)`
+        }
+
         return ( 
             <div id='cardContainer' className={classes.CardsContainer}>
-                <div className={classes.CardStrip} style={{
-                    'transform': `translateX(-${this.props.current*(100/this.props.numberOfTasks)}%)`
-                }}>
-                    {taskList}
+                <div className={classes.CardStrip} style={ slideStripStyle }>
+                    {taskPage}
                 </div>
             </div>
         );
     } 
 
     componentDidMount = () => {
-       let cardContainer = document.querySelector('#cardContainer');
-       let cardContainerWith = cardContainer.offsetWidth
-       let cards = cardContainer.firstChild.children;
-       console.log(cardContainerWith);
-       console.dir(cardContainer);
-       console.log(cards);
-        for (let i = 0; i < cards.length; i++){
-            cards[i].style.width = cardContainerWith + 'px';
-            console.log(cards[i]);
-        }
-       
+        this.resizeCards();
     }
 }
  
