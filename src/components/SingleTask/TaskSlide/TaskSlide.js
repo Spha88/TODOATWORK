7a26bdb232d7dd1616dd49bdfef23a2209151590
Siphamandla <mr.smehlomakulu@gmail.com>
2020-03-12 04:51:32 +0200

    Add js to auto size the with of the card slide to match that of the parent of slide

--- src/components/SingleTask/TaskSlide/TaskSlide.js
+++ src/components/SingleTask/TaskSlide/TaskSlide.js
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './TaskSlide.module.css';
 
-const taskSlide = props => {
+class TaskSlide extends Component {
 
-    let tasks = props.tasks;
-    // let key = props.key;
-    console.log(props.current);
-    console.log(props.numberOfTasks);
+    
 
-    let taskList = Object.keys(props.tasks).map( key => {
-        return (
-            <div key={key} className={classes.taskCard}>
-                <h3>{tasks[key].title}</h3>
-                <main>
-                    {tasks[key].details}
-                </main>
+    render = () => {
+        let tasks = this.props.tasks;
+        // let key = props.key;
+
+        let taskList = Object.keys(this.props.tasks).map( key => {
+            return (
+                <div key={key} className={classes.taskCard}>
+                    <h3>{tasks[key].title}</h3>
+                    <main>
+                        {tasks[key].details}
+                    </main>
+                </div>
+            );
+        });
+
+        return ( 
+            <div id='cardContainer' className={classes.CardsContainer}>
+                <div className={classes.CardStrip} style={{
+                    'transform': `translateX(-${this.props.current*(100/this.props.numberOfTasks)}%)`
+                }}>
+                    {taskList}
+                </div>
             </div>
         );
-    });
+    } 
 
-    return ( 
-        <div className={classes.CardsContainer}>
-            <div className={classes.CardStrip} style={{
-                'transform': `translateX(-${props.current*(100/props.numberOfTasks)}%)`
-            }}>
-                {taskList}
-            </div>
-        </div>
-    );
+    componentDidMount = () => {
+       let cardContainer = document.querySelector('#cardContainer');
+       let cardContainerWith = cardContainer.offsetWidth
+       let cards = cardContainer.firstChild.children;
+       console.log(cardContainerWith);
+       console.dir(cardContainer);
+       console.log(cards);
+        for (let i = 0; i < cards.length; i++){
+            cards[i].style.width = cardContainerWith + 'px';
+            console.log(cards[i]);
+        }
+       
+    }
 }
  
-export default taskSlide;
+export default TaskSlide;
